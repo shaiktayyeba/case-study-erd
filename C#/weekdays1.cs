ae0fbcb71c75c69ba56674ac90067aebb11520af
shaiktayyeba <89575550+shaiktayyeba@users.noreply.github.com>
2021-09-04 07:58:33 +0530

    Delete weekdays1.cs

--- C#/weekdays1.cs
+++
@@ -1,7 +0,0 @@
-﻿'weekdays1.exe' (CoreCLR: DefaultDomain): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.9\System.Private.CoreLib.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'weekdays1.exe' (CoreCLR: clrhost): Loaded 'D:\csharp\weekdays1\weekdays1\bin\Debug\net5.0\weekdays1.dll'. Symbols loaded.
-'weekdays1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.9\System.Runtime.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'weekdays1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.9\System.Console.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'weekdays1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.9\System.Threading.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-'weekdays1.exe' (CoreCLR: clrhost): Loaded 'C:\Program Files\dotnet\shared\Microsoft.NETCore.App\5.0.9\System.Text.Encoding.Extensions.dll'. Skipped loading symbols. Module is optimized and the debugger option 'Just My Code' is enabled.
-The program '[6380] weekdays1.exe' has exited with code 0 (0x0).
