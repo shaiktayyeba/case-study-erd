2b10b791e6d13d38cd12c6ea70e866bec9b191df
shaiktayyeba <89575550+shaiktayyeba@users.noreply.github.com>
2021-09-07 08:00:16 +0530

    Add files via upload

---
+++ C#/assignmentgetsetradius.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace assignmentgetsetradius
+{
+    class Circle
+    {
+        double Radius;
+        public void setRadius(double Radius)
+
+        {
+            this.Radius = Radius;
+        }
+        public double getRadius()
+        {
+            return this.Radius;
+        }
+        public double calcDiameter()
+        {
+            double diameter = 2 * Radius;
+            return diameter;
+        }
+        public double calcArea()
+        {
+            double area = Math.PI * Radius * Radius;
+            return area;
+        }
+    }
+    class CircleValues
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter the value of Radius : ");
+            double Radius = double.Parse(Console.ReadLine());
+            Circle circle = new Circle();
+            circle.setRadius(Radius);
+
+            Console.Write("The Entered Radius value is : ");
+            Console.WriteLine(circle.getRadius());
+            Console.Write("Diameter of the circle is : ");
+            Console.WriteLine(circle.calcDiameter());
+            Console.Write("Area of the circle is : ");
+            Console.WriteLine(circle.calcArea());
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
